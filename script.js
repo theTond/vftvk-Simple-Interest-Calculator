70ac6132c8916a131c83f704d9d0bf116602f315
Michele Tondato <75626428+theTond@users.noreply.github.com>
2022-07-17 16:35:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
+function compute() {
+  var principal = document.getElementById("principal").value;
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
 
-    var interest = principal * years * rate /100;
+  var interest = (principal * years * rate) / 100;
 
-    var year = new Date().getFullYear()+parseInt(years);
+  var year = new Date().getFullYear() + parseInt(years);
 
-    function updateRate() 
-    {
-        var rateval = document.getElementById("rate").value;
-        document.getElementById("rate_val").innerText=rateval;
-    }
+  if (principal < 1) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  } else {
+    document.getElementById("result").innerHTML =
+      "If you deposit <span class='yellow'>" +
+      principal +
+      "</span>,<br>at an interest rate of <span class='yellow'>" +
+      rate +
+      "%</span><br>You will receive an amount of <span class='yellow'>" +
+      interest +
+      "</span>,<br>in the year <span class='yellow'>" +
+      year +
+      "</span><br>";
+  }
 }
-        
+
+function alertMessage() {
+  var principalNum = document.getElementById("principal").value;
+
+  if (principalNum < 1) {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  }
+}
+
+function updateRate() {
+  if (document.getElementById("principal").value.length != 0) {
+    var rateval = document.getElementById("rate").value + "%";
+    document.getElementById("rate_val").innerText = rateval;
+  } else {
+    alert("Enter a positive number");
+    document.getElementById("principal").focus();
+  }
+}
